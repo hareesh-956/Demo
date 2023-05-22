8f04cd62bfda905aa7f69197700f865fb690f0ad
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-22 17:38:23 +0530

    [Hareesh]

---
+++ Demo.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Demo.cs
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Random h = new Random();
+            int hari = h.Next(0,2);
+           // Console.WriteLine(hari);
+            if (hari == 1)
+            {
+                Console.WriteLine("Emp is present");
+            }
+            else
+            {
+                Console.WriteLine("emp is absent");
+            }
+        }
+    }
+}
